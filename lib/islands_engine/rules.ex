7061264dd12dceff99d578362c1cc8c5dc79297d
mgmilton <matt.milton@gmail.com>
2018-08-23 10:03:48 -0700

    adds state machine for game

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -24,9 +24,29 @@ defmodule IslandsEngine.Rules do
      end
    end
 
-    def check(_state, _action), do: :error
+   def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}), do:
+      {:ok, %Rules{rules | state: :player2_turn}}
 
-    defp both_players_islands_set?(rules), do:
-      rules.player1 == :islands_set && rules.player2 == :islands_set
+  def check(%Rules{state: :player1_turn} = rules, {:win_check, win_or_not}) do
+      case win_or_not do
+         :no_win -> {:ok, rules}
+         :win       -> {:ok, %Rules{rules | state: :game_over}}
+      end
+  end
+
+  def check(%Rules{state: :player2_turn} = rules, {:guess_coordinate, :player2}), do:
+    {:ok, %Rules{rules | state: :player1_turn}}
+
+  def check(%Rules{state: :player2_turn} = rules, {:win_check, win_or_not}) do
+      case win_or_not do
+          :no_win -> {:ok, rules}
+          :win       -> {:ok, %Rules{rules | state: :game_over}}
+      end
+    end
+
+  def check(_state, _action), do: :error
+
+  defp both_players_islands_set?(rules), do:
+    rules.player1 == :islands_set && rules.player2 == :islands_set
 
 end
