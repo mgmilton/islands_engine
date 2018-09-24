257d240867b21d83baf009c3dccec11cfca6d69a
mgmilton <matt.milton@gmail.com>
2018-09-23 19:35:55 -0700

    add server callback

---
+++ lib/islands_engine/game.ex
@@ -0,0 +1,24 @@
+defmodule IslandsEngine.Game do
+  use GenServer
+
+  def handle_info(:first, state) do
+    IO.puts "This message has been handled by handle_info/2, matching on :first"
+    {:noreply, state}
+  end
+
+  def handle_call(:demo_call, _from, state) do
+    {:reply, state, state}
+  end
+
+  def demo_call(game) do
+    GenServer.call(game, :demo_call)
+  end
+
+  def handle_cast({:demo_cast, new_value}, state) do
+    {:noreply, Map.put(state, :test, new_value)}
+  end
+
+  def demo_cast(pid, new_value) do
+    GenServer.cast(pid, {:demo_cast, new_value})
+  end
+end
